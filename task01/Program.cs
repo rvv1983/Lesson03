66902e0fa6ca19223151233e00560050fc470b5a
Vladimir Rybin <rvv1983@yandex.ru>
2021-11-05 00:27:47 +0500

    решение готово

---
+++ task01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа
+
+Console.Write("Ведите число: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("{0} в квадрате {1}", a, Math.Pow(a, 2));
+    Console.ReadLine();
+
